cd7c300bcd88d0524e6b808386fbfbbf84f19ba8
shilovilya <shilovilya@gmail.com>
2025-04-13 10:41:46 -0400

    add redux and random experiment

--- src/app/providers/theme/chakra-provider.tsx
+++ src/app/providers/theme/chakra-provider.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ColorModeProvider, ColorModeProviderProps } from "@/shared/ui/color-mode/color-mode.tsx";
+import {
+  ColorModeProvider,
+  ColorModeProviderProps,
+} from "@/shared/ui/color-mode/color-mode.tsx";
 
 export function ChakraUIProvider(props: ColorModeProviderProps) {
   return (
